c3a01a4d7e60b29e69852d0a118e0cbc46b1153c
Pam Gaiguer <pamella.gaiguer@gmail.com>
2017-03-28 22:45:47 -0300

    finalizando home

--- app/js/main.js
+++ app/js/main.js
@@ -17,7 +17,6 @@
 		});
 	}
 
-
 	//  toggle menu
 	$( ".menu-block" ).hide();
 
@@ -77,11 +76,11 @@
 	//	PRELOADER
 
 	//<![CDATA[
-		$(window).load(function() { // makes sure the whole site is loaded
-			$('#status').fadeOut(); // will first fade out the loading animation
-			$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-			$('body').delay(350).css({'overflow':'visible'});
-		})
+		// $(window).load(function() { // makes sure the whole site is loaded
+		// 	$('#status').fadeOut(); // will first fade out the loading animation
+		// 	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+		// 	$('body').delay(350).css({'overflow':'visible'});
+		// })
 	//]]>
 
 	//	WOW
